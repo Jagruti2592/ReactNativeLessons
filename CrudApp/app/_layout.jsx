d8feaa179f08b4eec0befba1d0357f3ac772b879
Jagruti Chitte <81913740+Jagruti2592@users.noreply.github.com>
2025-11-04 20:16:45 -0500

    add title to homepage.

--- CrudApp/app/_layout.jsx
+++ CrudApp/app/_layout.jsx
@@ -9,7 +9,7 @@ export default function RootLayout() {
               <Stack>
                 <Stack.Screen 
                   name="index" 
-                  options={{headerShown: false}} 
+                  options={{headerShown: true, title: "Todo List"}} 
                 />  
                 </Stack>
           </SafeAreaProvider> 
